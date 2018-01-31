356b8c8a2aac66ca2e967cfb6e5007d0e097926a
elinsia <elinsia7@gmail.com>
2018-01-31 17:48:10 +0200

    Eslint

--- public/components/Map/Map.jsx
+++ public/components/Map/Map.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
 class Map extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     try {
       const mapOptions = {
         center: { lat: 49.0139, lng: 31.2858 },
         zoom: 6
       };
-      new google.maps.Map(document.getElementById('map'), mapOptions);
+      const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      console.log(map);
     } catch (error) {
       console.error(error);
     }
