10d03471ab6051f5342e73b4e3bc035db6651e15
LuckerOk <renger-red@ya.ru>
2018-02-06 13:31:14 +0200

    json-server

--- public/actions/questsActions.js
+++ public/actions/questsActions.js
@@ -1,17 +1,6 @@
-export const TOGGLE_QUEST = 'TOGGLE_QUEST';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
-export const SET_DISPLAY_FILTER = 'SET_DISPLAY_FILTER';
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-};
-
-export const DisplayFilter = {
-  OPEN: 'OPEN',
-  HIDE: 'HIDE'
-};
+import {
+  TOGGLE_QUEST, SET_VISIBILITY_FILTER, SET_DISPLAY_FILTER
+} from '../constants/actionTypes';
 
 export function toggleQuest(index) {
   return {
