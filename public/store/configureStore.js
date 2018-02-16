b20f76269c4361b248f20cfc5b7eb62162d4374e
LuckerOk <renger-red@ya.ru>
2018-02-16 12:13:51 +0200

    add redux devtools

--- public/store/configureStore.js
+++ public/store/configureStore.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createLogicMiddleware } from 'redux-logic';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import actions from '../actions';
 import reducer from '../reducers';
 
 const logicMiddleware = createLogicMiddleware(actions);
-const configureStore = createStore(reducer, applyMiddleware(logicMiddleware, logger));
+const configureStore = createStore(reducer, composeWithDevTools(applyMiddleware(logicMiddleware, logger)));
 
 export default configureStore;
