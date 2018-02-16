0e5c2a0c0033c358036ae17d802fd583a690f809
LuckerOk <renger-red@ya.ru>
2018-02-16 12:20:25 +0200

    delete logger

--- public/store/configureStore.js
+++ public/store/configureStore.js
@@ -1,11 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import { createLogicMiddleware } from 'redux-logic';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 import actions from '../actions';
 import reducer from '../reducers';
 
 const logicMiddleware = createLogicMiddleware(actions);
-const configureStore = createStore(reducer, composeWithDevTools(applyMiddleware(logicMiddleware, logger)));
+const configureStore = createStore(reducer, composeWithDevTools(applyMiddleware(logicMiddleware)));
 
 export default configureStore;
