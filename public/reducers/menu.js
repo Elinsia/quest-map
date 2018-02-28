a7604e8919fa727f9f14f72c67636942530a159b
dmitriy <dmitriy.bondarenko.it@gmail.com>
2018-02-28 16:51:01 +0200

    fix menu

--- public/reducers/menu.js
+++ public/reducers/menu.js
@@ -6,19 +6,9 @@ const initialState = false;
 export default function menu(state = initialState, action) {
   switch (action.type) {
     case OPEN_MENU:
-      // let newDiv = document.createElement('div');
-      // newDiv.classList.add('layer');
-      // document.body.appendChild(newDiv);
-      // console.log(newDiv);
-      // document.body.classList.add('modal-open');
       return true;
     case CLOSE_MENU:
     case LOCATION_CHANGE:
-      // let samDiv = document.querySelector('.layer');
-      // console.log(samDiv);
-      // document.body.classList.toggle('modal-open');
-      // document.body.removeChild(samDiv);
-      // document.body.classList.remove('modal-open');
       return false;
     default:
       return state;
